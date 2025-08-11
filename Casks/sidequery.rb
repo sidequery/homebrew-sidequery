6d57aaf24a9c41955bbfc35650f8ef1abe8bc777
Nico Ritschel <ritschel@gmail.com>
2025-08-11 12:54:50 -0700

    Add Sidequery 1.0.0 cask formula

---
+++ Casks/sidequery.rb
@@ -0,0 +1,17 @@
+cask "sidequery" do
+  version "1.0.0"
+  sha256 "2233adbab4d3dae2b870ef9c840e6c824f7ed4d9400044fe61cb213d4750033f"
+
+  url "https://github.com/sidequery/sidequery-native-releases/releases/download/v#{version}/Sidequery-#{version}.zip"
+  name "Sidequery"
+  desc "Fast native DuckDB client for macOS"
+  homepage "https://sidequery.com"
+
+  app "Sidequery.app"
+
+  zap trash: [
+    "~/Library/Application Support/Sidequery",
+    "~/Library/Preferences/com.sidequery.sidequery.plist",
+    "~/Library/Saved Application State/com.sidequery.sidequery.savedState"
+  ]
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# Homebrew Sidequery
+
+Homebrew tap for Sidequery - Fast native DuckDB client for macOS
+
+## Installation
+
+```bash
+brew tap sidequery/sidequery
+brew install --cask sidequery
+```
+
+## Uninstall
+
+```bash
+brew uninstall --cask sidequery
+```
