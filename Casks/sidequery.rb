e0506403099fa0346367dd6458520c31d15554e6
Nico Ritschel <ritschel@gmail.com>
2025-08-12 11:44:53 -0700

    Update to v0.0.4 (DMG distribution)

--- Casks/sidequery.rb
+++ Casks/sidequery.rb
@@ -1,8 +1,8 @@
 cask "sidequery" do
-  version "0.0.3"
-  sha256 "5030c80cfbed47f3b977f27000ae126a48a73c26f763de2071038be9186b2fdc"
+  version "0.0.4"
+  sha256 "07a8fa5c529c571b0e8606f3d7aeeb252f625682dbf72013ec1f2759069ae8ab"
 
-  url "https://github.com/sidequery/sidequery-native-releases/releases/download/v#{version}/Sidequery-#{version}.zip"
+  url "https://github.com/sidequery/sidequery-native-releases/releases/download/v#{version}/Sidequery-#{version}.dmg"
   name "Sidequery"
   desc "Fast native DuckDB client for macOS"
   homepage "https://sidequery.com"
