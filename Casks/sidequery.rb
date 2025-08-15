4a806158d596a58f4c3a81ea02c994b3d6db4502
Nico Ritschel <ritschel@gmail.com>
2025-08-15 00:41:01 -0700

    Update to v0.0.13 (DMG distribution)

--- Casks/sidequery.rb
+++ Casks/sidequery.rb
@@ -1,6 +1,6 @@
 cask "sidequery" do
-  version "0.0.12"
-  sha256 "34155a9aa5fac418b3465d7c8ce5dfa4483aed1a88e76d9fc042e8c08a493e3a"
+  version "0.0.13"
+  sha256 "7962bfc0901a6477e7e7bac8490ae2271523a8b9517f002b7f8011fe86b691de"
 
   url "https://github.com/sidequery/sidequery-native-releases/releases/download/v#{version}/Sidequery-#{version}.dmg"
   name "Sidequery"
